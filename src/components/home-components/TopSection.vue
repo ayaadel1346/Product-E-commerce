a907fab1379c651d606e1ad50e3057e387e71c79
Aya Adel <ayaadel1346@gmail.com>
2024-11-30 15:13:43 -0800

    blurhash

--- src/components/home-components/TopSection.vue
+++ src/components/home-components/TopSection.vue
@@ -1,30 +1,28 @@
 <template>
   <main class="min-h-[100vh] overflow-x-hidden my-[15vh] md:my-[8vh] lg:my-0 container-fluid grid grid-cols-1 lg:grid-cols-3">
-    
-   
     <aside class="w-full h-full relative">
       <video
-        v-if="videoLoading"
+        v-if="videoLoaded"
         autoplay
         muted
         loop
         playsinline
         ref="videoElement"
-        class="w-full h-[60vh] lg:min-h-[100vh] object-cover"
-      >
+        class="w-full h-[60vh] lg:min-h-[100vh] object-cover">
         <source :src="homeData.header.video" type="video/mp4" />
         <source :src="homeData.header.video" type="video/m4v" />
         <p>Your browser does not support the video tag.</p>
       </video>
 
       <BlurhashImg
-        v-if="!videoLoading"
+        v-if="!videoLoaded"
         hash="LPJ**a-=RjM{~qIUoft7RjofM_t7"
-        class="w-full h-[60vh] lg:min-h-[100vh] object-cover"
+        class="w-full h-[60vh] lg:min-h-[100vh] object-cover transition-opacity duration-1000 opacity-100"
+        :class="{'opacity-0': videoLoaded}"
       />
     </aside>
 
-  
+
     <section class="w-full flex justify-center items-center">
       <div class="flex-col gap-10 justify-center items-center w-full">
         <h1 class="mt-6 md:mt-10 lg:mt-0 md:text-[40px] lg:p-5 p-5 font-semibold text-[30px] md:leading-[70px] leading-[50px] text-center">
@@ -38,11 +36,11 @@
       </div>
     </section>
 
-   
+
     <aside class="w-full h-full hidden lg:block relative">
       <img
         v-if="imageLoaded"
-        :src="homeData.header.img"
+        :src="imageSrc"
         alt="right image"
         class="w-full h-[60vh] lg:min-h-[100vh] object-cover"
       />
@@ -50,11 +48,13 @@
       <BlurhashImg
         v-if="!imageLoaded"
         hash="L9OLr]~BXlI@JPx]tSDO%ggN_2OX"
-        class="w-full h-[60vh] lg:min-h-[100vh] object-cover"
+        class="w-full h-[60vh] lg:min-h-[100vh] object-cover transition-opacity duration-1000 opacity-100"
+        :class="{'opacity-0': imageLoaded}"
       />
     </aside>
 
   </main>
+  
 </template>
 
 <script setup lang="ts">
@@ -62,26 +62,38 @@ import { ref, toRefs, onMounted } from 'vue';
 import { useHomeStore } from "../../stores/HeaderStore";
 import BlurhashImg from "@/components/BlurhashImg.vue";
 
-
-const videoLoading = ref(false);
+const videoLoaded = ref(false);
 const imageLoaded = ref(false);
+const imageSrc = ref('');
 const { homeData } = toRefs(useHomeStore());
-
+const videoElement = ref(null);
 
 const checkIfMediaLoaded = () => {
- 
   if (homeData.value.header.video && homeData.value.header.video !== "") {
-    videoLoading.value = true;  
+    const video = document.createElement("video");
+    video.src = homeData.value.header.video;
+    video.onloadstart = () => {
+      videoLoaded.value = true;
+    };
+    video.onerror = () => {
+      videoLoaded.value = false;
+    };
   }
 
   if (homeData.value.header.img && homeData.value.header.img !== "") {
-    imageLoaded.value = true; 
+    imageSrc.value = homeData.value.header.img;
+    const img = new Image();
+    img.src = homeData.value.header.img;
+    img.onload = () => {
+      imageLoaded.value = true;
+    };
+    img.onerror = () => {
+      imageLoaded.value = false;
+    };
   }
 };
-
 
 onMounted(() => {
   checkIfMediaLoaded();
 });
-
 </script>
