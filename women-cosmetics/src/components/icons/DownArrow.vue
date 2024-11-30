d8741f29aa83485ee592e4a506233f263dcd1809
Aya Adel <156436119+ayaadel1346@users.noreply.github.com>
2024-11-30 11:54:30 -0800

    Delete women-cosmetics directory

--- women-cosmetics/src/components/icons/DownArrow.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-		<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m19 9l-7 6l-7-6" />
-	</svg>
-</template>
--- women-cosmetics/src/utils/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://api-woman-secrets.spider-te8.com/api', 
- 
-});
-
-export default instance;  
