fc0e0401476930f4f810d92743adf18ab4348481
Aya Adel <ayaadel1346@gmail.com>
2024-12-01 03:35:32 -0800

    add header translate

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,26 @@ import './style.css';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import router from './router/index';
+import { createI18n } from 'vue-i18n';
+import AR from './locale/ar.json';
 
 
-
+const i18n = createI18n({
+    locale:'EN',
+    messages:{
+        AR:AR
+    }
+  })
+  
+  
 const pinia = createPinia();
 
 const app = createApp(App);
 
 
 app.use(pinia);
+
+app.use(i18n)
 
 app.use(router)
 
