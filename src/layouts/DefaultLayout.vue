0d049e8604e8f2ad9658dd79673ff2403bf3abdb
Aya Adel <ayaadel1346@gmail.com>
2024-12-01 16:28:06 -0800

    remove footer from layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,11 +2,9 @@
     <div>
         <Header/>
         <RouterView />
-        <Footer/>
     </div>
 </template>
 
 <script setup>
 import Header from '@/components/header/Header.vue'
-import Footer from '@/components/Footer.vue'
 </script>
